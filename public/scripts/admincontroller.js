89b8be899091c2709030a2300599b06e428362b3
tonyzhangtz <tonyzhangtz@hotmail.ca>
2016-03-27 16:17:00 -0400

    Update admincontroller.js

--- public/scripts/admincontroller.js
+++ public/scripts/admincontroller.js
@@ -1,60 +1,79 @@
 app.controller("list", function($scope, $http) {
-      $scope.title = "Users List";
-      $scope.use = true;
-      convertList ('screen_name', { method: 'GET', url: 'users' });
+    $scope.title = "Users List";
+    $scope.use = true;
+    convertList ('screen_name', { method: 'GET', url: 'users' });
 
-      function convertList (type, req) {
-        var arr = [];
-        $http(req).success(function(data){
-          for (elem in data) {
-            if (type=='screen_name') {
-              arr.push(data[elem].screen_name);
-            }
-            else {
-              arr.push(data[elem].title);
-            }
+    function convertList (type, req) {
+      var arr = [];
+      $http(req).success(function(data){
+        for (elem in data) {
+          if (type=='screen_name') {
+            arr.push(data[elem].screen_name);
           }
-          $scope.data = arr;
-        }).error(function(){});
-      };
+          else {
+            arr.push(data[elem].title);
+          }
+        }
+        $scope.data = arr;
+      }).error(function(){});
+    };
 
-      $scope.getlist = function(list) {
-        if (list == 'users') {
-          $scope.title = "Users List";
-          $scope.use = true;
+    $scope.getlist = function(list) {
+      if (list == 'users') {
+        $scope.title = "Users List";
+        $scope.use = true;
+        convertList ('screen_name', { method: 'GET', url: 'users' });
+      }
+      else {
+        $scope.title = "Courses List";
+        $scope.use = false;
+        convertList ('title', { method: 'GET', url: 'courses' });
+      }
+    };
+
+    $scope.getFlagged = function() {
+      if ($scope.confirmed) {
+        if ($scope.title = "Users List") {
+          convertList ('screen_name', { method: 'GET', url: 'users/flagged' });
+        }
+        else {
+          cconvertList ('title', { method: 'GET', url: 'courses/flagged' });
+        }
+      }
+      else {
+        if ($scope.title = "Users List") {
           convertList ('screen_name', { method: 'GET', url: 'users' });
         }
         else {
-          $scope.title = "Courses List";
-          $scope.use = false;
+          cconvertList ('title', { method: 'GET', url: 'courses' });
+        }
+      }
+    };
+
+    $scope.delete = function(x) {
+      $scope.value = $scope.data[x];
+    };
+
+    $scope.removeItem = function () {
+      if($scope.title == "Users List") {
+        $http({
+          method: 'POST', 
+          url: 'delete/user', 
+          data: $.param({screen_name: $scope.value}),
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data){
+          convertList ('screen_name', { method: 'GET', url: 'users' });
+        }).error(function(){});
+      }
+      else {
+        $http({
+          method: 'POST', 
+          url: 'delete/course', 
+          data: $.param({title: $scope.value}),
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data){
           convertList ('title', { method: 'GET', url: 'courses' });
-        }
-      };
-
-      $scope.delete = function(x) {
-        $scope.value = $scope.data[x];
-      };
-
-      $scope.removeItem = function () {
-        if($scope.title == "Users List") {
-          $http({
-            method: 'POST', 
-            url: 'delete/user', 
-            data: $.param({screen_name: $scope.value}),
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-          }).success(function(data){
-            convertList ('screen_name', { method: 'GET', url: 'users' });
-          }).error(function(){});
-        }
-        else {
-          $http({
-            method: 'POST', 
-            url: 'delete/course', 
-            data: $.param({title: $scope.value}),
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-          }).success(function(data){
-            convertList ('title', { method: 'GET', url: 'courses' });
-          }).error(function(){});
-        }
-      };
-  });
+        }).error(function(){});
+      }
+    };
+});
