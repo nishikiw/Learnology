69013165e6e9367d08ce0d32b07f8c6a2bb2fb95
Sky-pie <sky.zhou@mail.utoronto.ca>
2016-03-27 22:11:55 -0400

    Added credits for regex used in protection from mongodb injection

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/search/res', urlencodedParser, function(req, res) {
     // Attack concepts from http://blog.websecurify.com/2014/08/hacking-nodejs-and-mongodb.html
     //console.log(req.body.searchBy +" " + req.body.terms );
     var terms = req.body.terms.toString(10); //Make sure it's a string
-    
+    // From http://stackoverflow.com/questions/13283470/regex-for-allowing-alphanumeric-and-space
     var regex = new RegExp("^[a-z\d\\-_\s]+$", "i"); // Allow alphanumeric characters, spaces, hyphens, underscores
     
     if(!regex.test(terms)){
