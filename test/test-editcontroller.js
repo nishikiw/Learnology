945cfbfb4a8e2534b8c9e79d7800c6f7de4ebe6c
Karunakaran Athavan <c2karuna@cdf.toronto.edu>
2016-04-06 19:28:31 -0400

    Basic Testing

---
+++ test/test-editcontroller.js
@@ -0,0 +1,12 @@
+describe('testing edit controller', function() {
+
+	var scope; 
+	var controller; 
+
+	beforeEach(module('learnologyApp')); 
+	
+	beforeEach(inject(function($rootScope, $controller) {
+		scope = $rootScope.$new(); 
+		controller = $controller('save', 
+
+});
